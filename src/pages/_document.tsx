70ef70f6014e34ef7aae7dd174ba1c2cd3180264
Mateus Croccoli <mateuscroccoli@MacBook-Air-de-Mateus.local>
2024-08-29 21:02:47 -0300

    dsddsadsas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,13 +11,12 @@ export default function Document() {
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
         <script>
-  window.pixelId = "66d0fdb34f04fa44c5955f2c";
-  var a = document.createElement("script");
-  a.setAttribute("async", "");
-  a.setAttribute("defer", "");
-  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-  document.head.appendChild(a);
-</script>
+          window.pixelId = "66d0fdb34f04fa44c5955f2c"; var a =
+          document.createElement("script"); a.setAttribute("async", "");
+          a.setAttribute("defer", ""); a.setAttribute("src",
+          "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+          document.head.appendChild(a);
+        </script>
       </Head>
       <body>
         <Main />
